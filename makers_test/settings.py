9e926b7f31cd7a2c2d4579ecdbf2cb3fff6557d0
Nurgazy <nurishok@Nurgazys-MacBook-Pro.local>
2020-08-06 13:34:50 +0600

    Adding hosts

--- makers_test/settings.py
+++ makers_test/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://localhost', 'http://127.0.0.1', 'https://nurbuster-django-test.herokuapp.com/']
 
 
 # Application definition
